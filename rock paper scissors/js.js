d746848402e5a4ee12fd3f18f907a1f632cf9448
Dayerino <154388150+Dayerino@users.noreply.github.com>
2024-04-17 11:51:43 +0100

    Add files via upload

---
+++ rock paper scissors/js.js
@@ -0,0 +1,60 @@
+let playerChoice 
+function popup(){
+    let input = prompt("Choose rock, paper, or scissors");
+    playerChoice = input
+    return playerChoice
+}
+popup();
+const computerOptions = ["rock", "paper", "scissors"];
+let computerChoice
+function AI(){
+let random = Math.floor(Math.random()* computerOptions.length);
+computerChoice = computerOptions[random];
+return computerChoice
+}
+AI();
+console.log(computerChoice);
+function playRound(){
+    switch(playerChoice){
+    case "rock":
+        switch (computerChoice) {
+            case "rock":
+                return "tie";
+            case "paper":
+                return "you lose";
+            case "scissors":
+                return "you win";
+            default:
+                break;
+        } break;
+    case "paper":
+            switch(computerChoice){
+                case "rock":
+                    return "you win";
+                case "paper":
+                    return "tie";
+                case "scissors":
+                    return "you lose";
+                default:
+                    break;
+            } break;
+        case "scissors":
+            switch(computerChoice){
+                case "rock":
+                    return "you lose";
+                case "paper":
+                    return "you win";
+                case "scissors":
+                    return "tie";
+                default:
+                    break;}
+        default:
+                    return "incorrect word"
+                }
+    
+}
+playRound();
+const result = playRound();
+console.log(result);
+// make the prompt = to player choice (done)
+//account for user to input paper and scissors
